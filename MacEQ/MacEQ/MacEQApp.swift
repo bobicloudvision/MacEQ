6b9350b054944eb94f417a8b99fdb3ee101b8c87
Bozhidar <bobicloudvision@gmail.com>
2025-10-20 18:32:29 +0300

    up

---
+++ MacEQ/MacEQ/MacEQApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacEQApp.swift
+//  MacEQ
+//
+//  Created by Bozhidar Slaveykov on 20.10.25.
+//
+
+import SwiftUI
+
+@main
+struct MacEQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
